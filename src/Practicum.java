fd71a5f97e410799a01a6c7ec00931665c6ea133
uzden <uzdenov02@gmail.com>
2025-01-13 15:44:02 +0300

    Переименовал основной класс Main в Practicum

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Practicum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
